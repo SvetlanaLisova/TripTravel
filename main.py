2c3e57c3b691774bbf7dd604dab9d1bd7c4db92c
SvetlanaLisova <70011123+SvetlanaLisova@users.noreply.github.com>
2021-02-14 11:35:48 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, json, jsonify, make_response
 from operator import itemgetter
+import webbrowser
 app = Flask(__name__)
 
 
@@ -16,7 +17,12 @@ def marsruti():
 def komp():
     return render_template('companions.html')
 
-
+@app.route('/forums')
+def forums():
+    return webbrowser.open_new_tab('http://bernuforums.lv/') 
+@app.route('/contacts')
+def contacts():
+    return  webbrowser.open_new_tab('https://www.startit.lv/lv/par-projektu')
 
      
 @app.route('/gdraugi')
